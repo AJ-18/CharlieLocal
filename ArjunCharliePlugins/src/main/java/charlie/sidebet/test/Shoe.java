71e961e81790918ea1c23b42d5dd8f983e5cb4a3
AJ-18 <arjun.suresh1@marist.edu>
2023-10-25 20:25:59 -0400

    Lab 8 Bugged

--- ArjunCharliePlugins/src/main/java/charlie/sidebet/test/Shoe.java
+++ ArjunCharliePlugins/src/main/java/charlie/sidebet/test/Shoe.java
@@ -63,6 +63,66 @@ public class Shoe extends charlie.card.Shoe {
         cards.add(new Card(8,Card.Suit.DIAMONDS));
 
         cards.add(new Card(3,Card.Suit.CLUBS));
+
+        // Test case #4
+        cards.add(new Card(7,Card.Suit.SPADES)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS)); //Dealers Downcard
+
+        cards.add(new Card(9,Card.Suit.SPADES)); //Player's 2nd Card
+        cards.add(new Card(10,Card.Suit.HEARTS)); //Dealer's Upcard
+
+        cards.add(new Card(3,Card.Suit.CLUBS)); //Player's Card on hit
+
+        // Test case #5
+        cards.add(new Card(9,Card.Suit.CLUBS)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS)); //Dealers Downcard
+
+        cards.add(new Card(7,Card.Suit.SPADES)); //Player's 2nd Card
+        cards.add(new Card(10,Card.Suit.DIAMONDS)); //Dealer's Upcard
+
+        cards.add(new Card(3,Card.Suit.CLUBS)); //Player's Card on hit
+
+        // Test case #6
+        cards.add(new Card(Card.KING,Card.Suit.SPADES)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS)); //Dealers Downcard
+
+        cards.add(new Card(Card.QUEEN,Card.Suit.SPADES)); //Player's 2nd Card
+        cards.add(new Card(8,Card.Suit.DIAMONDS)); //Dealer's Upcard
+
+        // Test case #7
+        cards.add(new Card(Card.KING,Card.Suit.DIAMONDS)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.SPADES)); //Dealers Downcard
+
+        cards.add(new Card(Card.QUEEN,Card.Suit.HEARTS)); //Player's 2nd Card
+        cards.add(new Card(8,Card.Suit.CLUBS)); //Dealer's Upcard
+
+        // Test case #8
+        cards.add(new Card(8,Card.Suit.DIAMONDS)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.SPADES)); //Dealers Downcard
+
+        cards.add(new Card(5,Card.Suit.HEARTS)); //Player's 2nd Card
+        cards.add(new Card(6,Card.Suit.CLUBS)); //Dealer's Upcard
+
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS)); //Dealer card
+
+        // Test case #9
+        cards.add(new Card(7,Card.Suit.DIAMONDS)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS)); //Dealers Downcard
+
+        cards.add(new Card(6,Card.Suit.HEARTS)); //Player's 2nd Card
+        cards.add(new Card(6,Card.Suit.DIAMONDS)); //Dealer's Upcard
+
+        cards.add(new Card(Card.KING,Card.Suit.DIAMONDS)); //Dealer Card
+
+        // Test case #10
+        cards.add(new Card(6,Card.Suit.DIAMONDS)); //Player's 1st Card
+        cards.add(new Card(Card.KING,Card.Suit.SPADES)); //Dealers Downcard
+
+        cards.add(new Card(8,Card.Suit.SPADES)); //Player's 2nd Card
+        cards.add(new Card(6,Card.Suit.CLUBS)); //Dealer's Upcard
+
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS)); //Dealer Card
+
     }
 
     /**
